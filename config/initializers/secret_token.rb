d85e6f277d03d7b93717ebf587902d15769b0b15
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2013-11-15 20:07:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kittenstore::Application.config.secret_key_base = '21f8ee5d806770f73cbba895061f458a841e3ef9c7a395e8e974a4f16d3c18a22804a5e6ab3e74f6cff7e606b3ce745cf9d5fd13fa1663d170615ed69dbe944b'
