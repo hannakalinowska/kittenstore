785cb0ad6cf52549f2eb640183eb998c8567aaac
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2013-11-16 18:02:48 +0000

    Prettifed stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def converted_file_size(size)
+    case size
+    when 0 .. 1024
+      "%d B" % size
+    when 1024 .. 1024 ** 2
+      "%d kB" % (size / 1024)
+    when 1024 ** 2 .. 1024 ** 3
+      "%.2f MB" % (size / (1.0 * (1024 ** 2)))
+    else
+      "%.2f GB" % (size / (1.0 * (1024 ** 3)))
+    end
+  end
 end
