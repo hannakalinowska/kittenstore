df13b2ab7f260051c95158d5f6303995de4909f1
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2013-11-16 12:36:32 +0000

    Tests to fill in later

---
+++ spec/features/images_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Images' do
+  it 'saves an uploaded image' do
+    pending
+  end
+
+  it 'scrapes an image from a remote url' do
+    pending
+  end
+
+  it 'deletes an image' do
+    pending
+  end
+end
